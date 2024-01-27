cfbd6a030a05c3e360b571d1482261d8795960ad
Bai Omar <bai.laye22@bathspa.ac.uk>
2024-01-27 20:17:03 +0000

    Fully complete... may need to add images but thats optional i feel

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 let ScrollGallery = document.getElementsByClassName("ScrollGallery");
 
+const HeaderItems = document.getElementsByClassName("headerItems");
+const HamburgerBtn = document.getElementById("HamburgerMenu");
+
 AnimateScrollGallery();
 
 function AnimateScrollGallery() {
@@ -7,5 +10,22 @@ function AnimateScrollGallery() {
         const Width = ((ScrollGallery[i].offsetWidth) *-1) + ScrollGallery[i].parentNode.clientWidth;
         //console.log(Width);
         ScrollGallery[i].style.translate = Width + "px";
+    }
+}
+
+function HamburgerToggle() {
+    const HeaderStatus = window.getComputedStyle(HeaderItems[0]).display;
+    if (HeaderStatus == "none") {
+        HamburgerBtn.setAttribute("src", "./Assets/Imgs/Ham_Open.png");
+        for (let index = 0; index < HeaderItems.length; index++) {
+            HeaderItems[index].style.display = "flex"
+        }
+        HamburgerClosed = false;
+    } else {
+        HamburgerBtn.setAttribute("src", "./Assets/Imgs/Ham_Closed.png");
+        for (let index = 0; index < HeaderItems.length; index++) {
+            HeaderItems[index].style.display = "none"
+        }
+        HamburgerClosed = true;
     }
 }
