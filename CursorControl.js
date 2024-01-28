51c5539acb8b0799ce6acb92d06625bdc7144f80
Bai Omar <bai.laye22@bathspa.ac.uk>
2024-01-28 20:54:50 +0000

    commenting code

--- CursorControl.js
+++ CursorControl.js
@@ -1,7 +1,5 @@
-// const Cursor = document.getElementById('Cursor');
-
 document.addEventListener('mousemove', e => {
-    // let CursorSize = parseInt(getComputedStyle(Cursor).width, 10);
+    // sets the css variables to the mouse value everytime the mouse is moved within the site
     document.documentElement.style.setProperty('--CursorTop', e.clientY + "px");
     document.documentElement.style.setProperty('--CursorLeft', e.clientX + "px");
 })
--- index.js
+++ index.js
@@ -14,15 +14,17 @@ function AnimateScrollGallery() {
 }
 
 function HamburgerToggle() {
+    if (parseInt(window.getComputedStyle(document.body).width) > 460) return;
     const HeaderStatus = window.getComputedStyle(HeaderItems[0]).display;
+
     if (HeaderStatus == "none") {
-        HamburgerBtn.setAttribute("src", "./Assets/Imgs/Ham_Open.png");
+        HamburgerBtn.setAttribute("src", "./Assets/Imgs/Ham_Open.webp");
         for (let index = 0; index < HeaderItems.length; index++) {
             HeaderItems[index].style.display = "flex"
         }
         HamburgerClosed = false;
     } else {
-        HamburgerBtn.setAttribute("src", "./Assets/Imgs/Ham_Closed.png");
+        HamburgerBtn.setAttribute("src", "./Assets/Imgs/Ham_Closed.webp");
         for (let index = 0; index < HeaderItems.length; index++) {
             HeaderItems[index].style.display = "none"
         }
